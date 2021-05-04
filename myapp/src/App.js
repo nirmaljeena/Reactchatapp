78e9f93f1fe0f137e6bfb25ecc5dba3e3ec14fa7
nirmal jeena <nirmaljeena001@gmail.com>
2021-05-04 18:07:46 +0530

    hello

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,7 +4,7 @@ const App = () =>{
   return (
     <div>
       <h1>hello nirmal</h1>
-      <h2>hello nidhi</h2>
+      <h2>hello nidhi She is my sister</h2>
       <h3>hello rekha</h3>
       <h3>hello rekha</h3>
     </div>
